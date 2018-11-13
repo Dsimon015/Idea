671f62593fb200ecb28ff271586cf3d34bd3aa15
Dsimon015 <acze1506@gmail.com>
2018-11-13 17:45:00 +0100

    Clear main file

--- 123321.py
+++ 123321.py
@@ -3,8 +3,4 @@ from tkinter import *
 window = Tk()
 label = Label(window, text='tekst')
 
-def hallo:
-    label['text'] ('Inny')
-btn = Button(window, text='Click', command=hello)
-btn.pack()
-label.pack()
+window.mainloop()
